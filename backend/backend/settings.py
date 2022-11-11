0182122561b39dcaf86112bb0980dfa5ae3ca04b
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-11-12 01:19:56 +0300

    v1.2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -133,10 +133,10 @@ USE_TZ = True
 
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'backend_static')
 
 MEDIA_URL = '/backend_media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'backend_media')
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': (
