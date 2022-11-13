8e0f1ed5169ef3ff68116d528c4d359d75c97398
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-11-13 16:46:51 +0300

    v1.3

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -2,12 +2,18 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('users.urls')),
     path('api/', include('tags.urls')),
     path('api/', include('recipes.urls')),
+    path(
+        'redoc/',
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
+    ),
 ]
 
 if settings.DEBUG:
