9b4bdc9e4a6f4d30ecb20f99e2ea4549d98894df
Ярослав Лошкарев <dfdfdf.dfdf.1985@mail.ru>
2022-11-13 14:34:37 +0300

    v1.2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,32 +37,6 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-DJOSER = {
-    "LOGIN_FIELD": 'email',
-    'USER_ID_FIELD': 'id',
-    'PASSWORD_RESET_CONFIRM_URL': 'set_password/{uid}/{token}',
-    "SEND_ACTIVATION_EMAIL": False,
-    'HIDE_USERS': False,
-    'SERIALIZERS': {
-
-        'user': 'users.serializers.CurrentUserSerializer',
-        'current_user': 'users.serializers.CurrentUserSerializer',
-    },
-    'PERMISSIONS': {
-        'activation': ['rest_framework.permissions.AllowAny'],
-        'password_reset_confirm': ['rest_framework.permissions.AllowAny'],
-        'set_password': ['djoser.permissions.CurrentUserOrAdmin'],
-        'username_reset': ['rest_framework.permissions.AllowAny'],
-        'username_reset_confirm': ['rest_framework.permissions.AllowAny'],
-        'set_username': ['djoser.permissions.CurrentUserOrAdmin'],
-        'user_create': ['rest_framework.permissions.AllowAny'],
-        'user_delete': ['djoser.permissions.CurrentUserOrAdmin'],
-        'user': ['rest_framework.permissions.AllowAny'],
-        'user_list': ['rest_framework.permissions.AllowAny'],
-        'token_create': ['rest_framework.permissions.AllowAny'],
-        'token_destroy': ['rest_framework.permissions.IsAuthenticated'],
-    }
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
